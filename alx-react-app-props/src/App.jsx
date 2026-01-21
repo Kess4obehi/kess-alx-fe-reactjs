648a1bbcd0b5de6fcdca5f7c1db1f41eab9d401d
kess4obehi <kessiena4excellence@gmail.com>
2026-01-21 10:18:28 +0100

    correction made

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -19,23 +19,23 @@ function App() {
 
   return (
     <>
-      <div>
-        <WelcomeMessage />
-        <UserContext.Provider value={userData}>
+      <UserContext.Provider value={userData}>
+        <div>
+          <WelcomeMessage />
           <ProfilePage />
-        </UserContext.Provider>
 
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <Header />
-        <MainContent />
-        <Footer />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+          <UserProfile />
+          <Header />
+          <MainContent />
+          <Footer />
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+      </UserContext.Provider>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
